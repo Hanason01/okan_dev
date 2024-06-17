70fdada1d112f92ca1b96a184c98ddd0d677a888
pucha <pucha@puchanoMac-mini.local>
2024-06-17 16:12:30 +0900

    デプロイ修正(seedファイル）

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,17 @@ PomodoroSetting.find_or_create_by!(
   set_title: "デフォルトポモドーロ",
   breaks: 5
 )
+
+# OkanReprimandのインスタンスを追加
+OkanReprimand.find_or_create_by!(id: 1, body: "あんた！最後までやらなあかんやんか！")
+OkanReprimand.find_or_create_by!(id: 2, body: "もおほんまに、お母さん恥ずかしいわ！")
+OkanReprimand.find_or_create_by!(id: 3, body: "余裕ぶっこいてたらアカンで！ほんま！")
+OkanReprimand.find_or_create_by!(id: 4, body: "どこ行くんや！まだ終わってへんがな！")
+OkanReprimand.find_or_create_by!(id: 5, body: "真面目にしい！お母さんもう先寝るで！")
+OkanReprimand.find_or_create_by!(id: 6, body: "根性がなっとらんわ！しゃっきあげたろか！")
+OkanReprimand.find_or_create_by!(id: 7, body: "なんやて？シャキッとしいなあんたホンマ！")
+OkanReprimand.find_or_create_by!(id: 8, body: "コルァ！集中しな！あんたもう飯抜きや！")
+OkanReprimand.find_or_create_by!(id: 9, body: "どこ行くねん！ほなお母さんも帰るわ！")
+OkanReprimand.find_or_create_by!(id: 10, body: "これやさかい怠けもんは！ええ加減にし！")
+OkanReprimand.find_or_create_by!(id: 11, body: "あんたどないなってん！お母さん悲しいわ！")
+OkanReprimand.find_or_create_by!(id: 12, body: "作業中でっしゃろ？そんなんおまへんがな！")
